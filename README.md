a1ffbe6b010f604a5275e6b5ad301419b5cedcdf
Ivan Smoliarov <i.a.smoliarov@gmail.com>
2023-08-24 16:52:30 +0300

    Updated example using new version

--- README.md
+++ README.md
@@ -75,3 +75,34 @@ Video example:
 
 https://github.com/Smoliarick/cypress-discord-webhook-integration-example/assets/104084410/640ff1e8-3ccc-434c-a038-5d57ac8832b0
 
+If you want to generate 1 PNG file with message for each spec file, use `after:spec` instead of `after:run`:
+
+```js
+on('after:spec', async (results) => {
+  await afterRunHook();
+
+  // --------------------required part------------------------------
+  const sendToDiscordWebhook = require('cypress-discord-webhook-integration'); // import lib
+  const webhookURL = process.env.WEBHOOK_URL; // REQUIRED: Webhook URL for Discord
+  const files = ['./cypress/reports/html/index.html']; // REQUIRED: File paths
+  // --------------------required part------------------------------
+
+  // --------------------custom data------------------------------
+  const customUsername = 'Custom Username for Bot'; // Custom name for Bot's username in Discord
+  const customMessage = 'Custom message for Bot'; // Custom message for Bot's message in Discord
+  const customAvatar = 'https://cdn.sanity.io/images/o0o2tn5x/production/13b9c8412093e2f0cdb5495e1f59144967fa1664-512x512.jpg'; // Custom avatar URL for Bot in Discord
+  // --------------------custom data------------------------------
+
+  // --------------------required part------------------------------
+  // Using function
+  await sendToDiscordWebhook(
+    webhookURL,     // required variable
+    files,          // required variable
+    customMessage,  // set to undefined if you don't use custom message, but use custom avatar, custom username or convertHtmlToPng functionality
+    customUsername, // set to undefined if you don't use custom username, but use custom avatar or convertHtmlToPng functionality
+    customAvatar,   // set to undefined if you don't use custom message, but use convertHtmlToPng functionality
+    true,           // if you want to convert HTML files to PNG set it as true, or remove it if you don't want to use this functionality
+  );
+  // --------------------required part------------------------------
+});
+```
--- cypress.config.js
+++ cypress.config.js
@@ -18,6 +18,7 @@ module.exports = defineConfig({
         await beforeRunHook(details);
       });
 
+      // If you want to see 1 message for 1 full run use 'after:spec' function
       on('after:run', async (results) => {
         await afterRunHook();
 
@@ -45,6 +46,37 @@ module.exports = defineConfig({
         );
         // --------------------required part------------------------------
       });
+
+      // If you want to see 1 message for 1 spec use 'after:spec' function
+      // Uncomment these lines for see results, and comment 'after:run' function
+      /* on('after:spec', async (results) => {
+        await afterRunHook();
+
+        // --------------------required part------------------------------
+        const sendToDiscordWebhook = require('cypress-discord-webhook-integration'); // import lib
+        const webhookURL = process.env.WEBHOOK_URL; // REQUIRED: Webhook URL for Discord
+        const files = ['./cypress/reports/html/index.html']; // REQUIRED: File paths
+        // --------------------required part------------------------------
+
+        // --------------------custom data------------------------------
+        const customUsername = 'Custom Username for Bot'; // Custom name for Bot's username in Discord
+        const customMessage = 'Custom message for Bot'; // Custom message for Bot's message in Discord
+        const customAvatar = 'https://cdn.sanity.io/images/o0o2tn5x/production/13b9c8412093e2f0cdb5495e1f59144967fa1664-512x512.jpg'; // Custom avatar URL for Bot in Discord
+        // --------------------custom data------------------------------
+
+        // --------------------required part------------------------------
+        // Using function
+        await sendToDiscordWebhook(
+          webhookURL,     // required variable
+          files,          // required variable
+          customMessage,  // set to undefined if you don't use custom message, but use custom avatar, custom username or convertHtmlToPng functionality
+          customUsername, // set to undefined if you don't use custom username, but use custom avatar or convertHtmlToPng functionality
+          customAvatar,   // set to undefined if you don't use custom message, but use convertHtmlToPng functionality
+          true,           // if you want to convert HTML files to PNG set it as true, or remove it if you don't want to use this functionality
+        );
+        // --------------------required part------------------------------
+      });
+      */
       return config;
     },
     specPattern: 'cypress/e2e/**/*.spec.{js, jsx, ts, tsx}',
